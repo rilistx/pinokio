0c13236a73b9c753132dd317680144afa8b7eaa1
Kyrylo Kyryliuk <rilistx@gmail.com>
2024-12-22 20:29:48 +0200

    init

---
+++ core/database/engine.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+from core.settings.configer import postgres_url
+
+
+async_engine = create_async_engine(
+    url=postgres_url,
+    echo=True,
+)
+
+async_session = async_sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
